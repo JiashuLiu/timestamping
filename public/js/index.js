5d4f4e717eee787a1a65da6d5fa6c6a6c9526ccb
JiashuLIu <shshshjs1234@gmail.com>
2016-06-12 01:27:41 +0200

    modifyed post entry point

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,10 @@ app.controller('MainController', ['$scope', '$http', function($scope,$http) {
 $scope.getIt=function(){
   var req = {
    method: 'POST',
-   url: 'http://localhost:3000/keywords',
+   url: 'https://timestampingg.herokuapp.com/keywords',
    data: { date: $scope.keywords }
   }
-  
+
   $http(req).then(
     function(res){
     $scope.tada=res.data;
